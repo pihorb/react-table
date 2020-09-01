18a61cac8da7e558b202a99da414e3a7e5f77276
pihorb <ihorpylypyak@gmail.com>
2020-09-01 17:02:31 +0300

    better formating

--- src/Components/TableSearch/TableSearch.js
+++ src/Components/TableSearch/TableSearch.js
@@ -10,14 +10,19 @@ const TableSearch = (props) => {
   return (
     <div className="input-group mb-3 mt-3">
       <div className="input-group-prepend">
-        <button className="btn btn-outline-secondary" onClick={() => props.search(value)}>Search</button>
+        <button
+          className="btn btn-outline-secondary"
+          onClick={() => props.search(value)}
+        >
+          Search
+        </button>
       </div>
       <input
         type="text"
         className="form-control"
         value={value}
         onChange={valueChangeHandler}
-        />
+      />
     </div>
   )
 }
